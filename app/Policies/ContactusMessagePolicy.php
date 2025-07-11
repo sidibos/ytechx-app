d9f4a1b869ba577558d618ed149836ad9561409f
Moussa Sidibe <sidibemousse@hotmail.com>
2025-07-11 16:47:08 +0100

    fix: policy bug

--- app/Policies/ContactusMessagePolicy.php
+++ app/Policies/ContactusMessagePolicy.php
@@ -6,7 +6,7 @@ use App\Models\ContactUsMessage;
 use App\Models\User;
 use Illuminate\Auth\Access\Response;
 
-class ContactusMessagePolicy extends BasePolicy
+class ContactUsMessagePolicy extends BasePolicy
 {
     /**
      * Determine whether the user can view any models.
